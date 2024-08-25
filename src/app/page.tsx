66ab1f9b92031cdb5535f3ade8b7cb596164b0ee
mazhar-iqbal <mazhar.iqbal223@gmail.com>
2024-08-25 12:17:48 +0500

    react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const page = () => {
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          Sarah Dayan
+          mazhar iqbal
         </div>
         <div className="text-slate-700 dark:text-slate-500">
           Profession Teaching Mazhar 
