cfd6e5112edbc6a881c5acb630397bf7408fc9b3
mazhar-iqbal <mazhar.iqbal223@gmail.com>
2024-08-25 11:57:59 +0500

    react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>mazhar iqbal</div>
+    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"/>
+    <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+      <blockquote>
+        <p className="text-lg font-medium">
+          “hi i am mazhar iqbal from yar khan. i am a generative ai student. By professional i am a teacher.”
+        </p>
+      </blockquote>
+      <figcaption className="font-medium">
+        <div className="text-sky-500 dark:text-sky-400">
+          Sarah Dayan
+        </div>
+        <div className="text-slate-700 dark:text-slate-500">
+          Profession Teaching Mazhar 
+        </div>
+      </figcaption>
+    </div>
+  </figure></div>
   )
 }
 
